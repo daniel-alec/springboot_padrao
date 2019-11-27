21fd8102d7b34b9c90cb5b7ec8a6939d5c76436d
Daniel <daniel.alexandrec@gmail.com>
2019-11-27 03:14:55 -0300

    Thymeleaf tested.
    Signed-off-by: Daniel <daniel.alexandrec@gmail.com>

---
+++ src/main/java/com/example/padrao/controller/OrgaoController.java
@@ -0,0 +1,37 @@
+package com.example.padrao.controller;
+
+import com.example.padrao.repository.OrgaoRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class OrgaoController {
+    private OrgaoRepository orgaoRepository;
+
+    public OrgaoController(OrgaoRepository orgaoRepository) {
+        this.orgaoRepository = orgaoRepository;
+    }
+
+    @GetMapping("/orgaos")
+    public String orgaosPage(HttpServletRequest request, Model model) {
+
+        int page = 0; //default page number is 0 (yes it is weird)
+        int size = 5; //default page size is 5
+
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+
+        model.addAttribute("orgaos", orgaoRepository.findAll(PageRequest.of(page, size)));
+
+        return "orgaos";
+    }
+}
